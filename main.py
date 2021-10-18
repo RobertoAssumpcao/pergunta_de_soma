d093bd847c59a87025d4dd7f4c2e66abe19158e5
Roberto Assumpcao <robertoassumpcao1999@gmail.com>
2021-10-17 21:29:45 -0300

    Primeira versão

---
+++ main.py
@@ -0,0 +1,44 @@
+#import
+import random
+
+#gerando numero
+numero_a = random.randrange(1, 100)
+numero_b = random.randrange(1, 100)
+
+# soma numero
+resultado = numero_a + numero_b
+
+#pergunta
+resposta = int(input(f"Quanto é {numero_a} + {numero_b} ? \n"))
+
+#resposta
+print(f"A resposta é {resultado}")
+
+#contando acerto
+if resposta == resultado:
+    acertos = 0
+    acertos = acertos + 1
+    
+    
+# ---------------------------------------------------------------- 2 -------------------------------
+
+#gerando numero
+numero_a = random.randrange(1, 100)
+numero_b = random.randrange(1, 100)
+
+# soma numero
+resultado = numero_a + numero_b
+
+#pergunta
+resposta = int(input(f"Quanto é {numero_a} + {numero_b} ? \n"))
+
+#resposta
+print(f"A resposta é {resultado}")
+
+#contando acerto
+if resposta == resultado:
+    acertos = acertos + 1
+    
+print(f"Voce acertou {acertos} vezes")
+
+
